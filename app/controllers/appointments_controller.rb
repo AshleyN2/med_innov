891f04cb8cfe934b15fd750cb7de88204065e5d0
Ashley <ashleynjeri7@gmail.com>
2022-12-12 18:54:21 +0300

    Doctor & Appointment Controllers Updated

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,3 +1,31 @@
 class AppointmentsController < ApplicationController
+    
+    def index
+        appointments = Appointment.all
+        render json: appointments, status: :ok
+    end
 
+    def show
+        appointment = Appointment.find(params[:id])
+        render json: appointment, status: :ok
+    end
+
+    def create
+        patient = Patient.find(params[:patient_id])
+        hospital = Hospital.find(params[:hospital_id])
+        appointment = patient.appointments.create!(appointment_params) || hospital.appointments.create!(appointment_params)
+        render json: appointment, status: :created
+    end
+
+    def destroy
+        appointment = Appointment.find(params[:id])
+        appointment.destroy
+        render json: {message:['Successfully deleted!']}
+    end
+
+    private
+
+    def appointment_params
+        params.permit(:patient_id, :doctor_id, :hospital_id, :diagnosis, :date)
+    end
 end
--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,2 +1,36 @@
 class DoctorsController < ApplicationController
+
+    def index
+        doctors = Doctor.all
+        render json: doctors, status: :ok
+    end
+
+    def show
+        doctor = Doctor.find(params[:id])
+        render json: doctor, status: :ok
+    end
+
+    def create
+        hospital = Hospital.find(params[:hospital_id])
+        doctor = hospital.doctors.create!(doctor_params)
+        render json: doctor, status: :created
+    end
+
+    def update
+        doctor = Doctor.find(params[:id])
+        doctor.update!(doctor_params)
+        render json: doctor, status: :ok
+    end
+
+    def destroy
+        doctor = Doctor.find(params[:id])
+        doctor.destroy
+        render json: {message:['Successfully deleted!']}
+    end
+
+    private
+
+    def doctor_params
+        params.permit(:name, :age, :speciality, :hospital_id)
+    end
 end
