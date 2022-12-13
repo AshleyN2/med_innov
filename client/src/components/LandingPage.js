d658aa4ef6e47f7ff3620c0de0ce3a4865e204f5
Ashley <ashleynjeri7@gmail.com>
2022-12-13 13:17:15 +0300

    landing page updated with patient & hospital login buttons

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -1,19 +1,15 @@
 import React from 'react'
-import { useNavigate } from 'react'
-
-
+import { useNavigate } from 'react-router-dom'
 
 function LandingPage() {
   const navigate = useNavigate()
-  return (
+return (
     <div>
-
+        <h1>Landing Page</h1>
         <button onclick={()=>{navigate('/patient_login')}}>Patient Login</button>
         <button  onclick={()=>{navigate('/hospital_login')}}>Hospital Login</button>
     </div>
   )
-
-
 }
 
 export default LandingPage
