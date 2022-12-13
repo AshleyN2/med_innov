73c5d8a8fb0b959ff705224eafa774c6e492d44c
Ashley <ashleynjeri7@gmail.com>
2022-12-13 18:47:36 +0300

    hospital signup & login added

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import React,{useState, useEffect} from 'react';
-import LandingPage from './components/LandingPage';
-import PatientLogin from './components/PatientLogin';
-import PatientHomePage from './components/PatientHomePage';
-import PatientSignup from './components/PatientSignup'
-import HospitalLogin from './components/HospitalLogin'
-import HospitalSignup from './components/HospitalSignup'
 import {Routes, Route} from 'react-router-dom'
+import LandingPage from './components/LandingPage';
+import PatientLogIn from './components/PatientLogIn';
+import PatientSignUp from './components/PatientSignUp';
+import HospitalLogIn from './components/HospitalLogIn';
+import HospitalSignUp from './components/HospitalSignUp';
+import PatientHomePage from './components/PatientHomePage';
+import HospitalDashboard from './components/HospitalDashboard';
+import AppointmentForm from './components/AppointmentForm';
+import MyAppointments from './components/MyAppointments';
+
 
 function App() {
 
@@ -15,7 +19,7 @@ function App() {
   const [patient, setPatient] = useState([])
 
   const getPatient = async () => {
-    const response = await fetch('/patient')
+    const response = await fetch(`/patients/${patient.id}`)
     const data = await response.json()
     setPatient(data)
   }
@@ -28,7 +32,7 @@ function App() {
 
   const [hospital, setHospital] = useState([])
   const getHospital = async () => {
-    const response = await fetch('/hospital')
+    const response = await fetch(`/hospitals/${hospital.id}`)
     const data = await response.json()
     setHospital(data)
   }
@@ -54,9 +58,14 @@ function App() {
     <div className="App">
       <Routes>
         < Route path='/' element={<LandingPage/>} />
-        < Route path='/patient_login' element={<PatientLogin setPatient={setPatient} />} />
+        < Route path='/patient_login' element={<PatientLogIn setPatient={setPatient} />} />
+        < Route path='/patient_signup' element={<PatientSignUp  setPatient={setPatient} />} />
+        < Route path='/hospital_login' element={<HospitalLogIn setHospital={setHospital}  />} />
+        < Route path='/hospital_signup' element={<HospitalSignUp setHospital={setHospital}   />} />
         < Route path='/patient_homepage' element={<PatientHomePage setPatient={setPatient} hospitals={hospitals} patient={patient} />} />
-        < Route path='/hospital_login' element={<HospitalLogin setHospital={setHospital}  />} />
+        < Route path='/hospital_dashboard' element={<HospitalDashboard setHospital={setHospital} hospital={hospital} />} />
+        < Route path='/appointment_form/:id' element={<AppointmentForm setPatient={setPatient} patient={patient} hospitals={hospitals} setHospital={setHospital} />} />
+        < Route path='/my_appointments' element={<MyAppointments patient={patient} />} />
       </Routes>
     
     </div>
